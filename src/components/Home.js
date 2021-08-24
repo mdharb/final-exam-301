0d09cb82bb57977c83a074f638330e6a0603f788
mdharb <md20.harb21@gmail.com>
2021-08-24 13:53:48 +0300

    pushing frontend code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 class Home extends React.Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    serverLink: process.env.REACT_APP_SERVER_URL,
+    flowers: [],
+  }
+}
+
+componentDidMount = async () => {
+  const url = "http://localhost:3030/get-data"
+  const getFlowers = await axios.get(url)
+this.setState({
+  flowers: getFlowers.data
+})
+}
+
 
   render() {
     return (
       <>
         <h1>API Flowers</h1>
+        
       </>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-// TODO: wrap everything in Auth0
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-9dv7u82q.eu.auth0.com"
+    clientId="byZtBuxtMmu86n9hpnyOz3WQbPpy47I2"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
